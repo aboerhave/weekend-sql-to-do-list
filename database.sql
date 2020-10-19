cddb3b63cfe553d289237d1a13cdb066d60ee014
Adam Boerhave <arboerhave@gmail.com>
2020-10-18 22:46:48 -0500

    updated the database.sql file to include table creation command

--- database.sql
+++ database.sql
@@ -1,8 +1,11 @@
+-- This is the database.sql file for the weekend assignment for week 9 of Prime Digital Academy
+-- for Adam Boerhave, to make a to do list app, created 10/16/2020 - 10/18/2020
+
+-- This is the sql command to create the database table used
 CREATE TABLE "tasks"(
 "id" serial primary key,
 "task_description" varchar(500) NOT NULL,
 "category" varchar(30),
-"priority" varchar(30),
+"priority" integer,
 "complete_status" boolean
 );
-
